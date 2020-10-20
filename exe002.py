47d3e49d0d4f70c191a9d3df1fc8139e3050ccf5
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-20 10:52:44 -0300

    add exercicio 002.update

--- exe002.py
+++ exe002.py
@@ -1,4 +1,7 @@
 #Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
 nome = input('Qual seu nome? ')
-print('Bem vindo(a), ' + nome)
+print('Bem vindo(a), {}!'.format(nome))
+
+#outro formato de print simples
+#print('Bem vindo(a), ' + nome)
