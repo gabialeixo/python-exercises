31fc37a6d20a92e50e651de320737701cb09edd8
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-20 12:00:36 -0300

    add exercicio 003

---
+++ exe003.py
@@ -0,0 +1,10 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+print('A soma entre {} e {} vale: {}!'.format(num1, num2, soma))
+
+#forma antiquada
+#print('A soma desses valores é: ', soma)
+#print('A soma entre', num1, 'e', num2, 'vale:', soma)
