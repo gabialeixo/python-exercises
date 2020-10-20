9393118db229f26ec2b25a8fbe3ec94e0436048a
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-20 16:09:46 -0300

    add exercicio 009

---
+++ exe009.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+num = int(input('Digite um número: '))
+n1 = 1 * num
+n2 = 2 * num
+n3 = 3 * num
+n4 = 4 * num 
+n5 = 5 * num
+n6 = 6 * num
+n7 = 7 * num
+n8 = 8 * num
+n9 = 9 * num
+n10 = 10 * num
+print('A tabuada do {} é: \n{} * 1 = {} \n{} * 2 = {} \n{} * 3 = {} \n{} * 4 = {} \n{} * 5 = {} \n{} * 6 = {} \n{} * 7 = {} \n{} * 8 = {} \n{} * 9 = {} \n{} * 10 = {}'.format(num, num, n1, num, n2, num, n3, num, n4, num, n5, num, n6, num, n7, num, n8, num, n9, num, n10))
