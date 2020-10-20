ccdd8ee6b35a8710d2b81c77762c65b7c0c46a7b
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-20 16:16:20 -0300

    add exercicio 010

---
+++ exe010.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+#Considere US$1.00 =  R$3.27
+
+din = float(input('Qaunto dinheiro você tem na carteira: '))
+dol = din * 3.27
+print('Tenho {:.2f} reais, que equivalem a {:.2f} dólares!'.format(din, dol))
