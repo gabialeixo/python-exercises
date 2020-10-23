390655cf9e325d20c7757d2f581c4c6a4a3acbfd
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-22 22:44:49 -0300

    update exercicio 024

--- exe024.py
+++ exe024.py
@@ -1,5 +1,8 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com nome o "SANTO"
 
 cidade = input('Digite o nome da sua cidade: ')
-div = cidade.split()
-print('Santo' in div[0])
+print(cidade.startswith('Santo'))
+
+
+#div = cidade.split()
+#print('Santo' in div[0])
