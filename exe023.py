1e60134cdbfcf29810b9de39033da6e34e2d335a
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-22 21:52:44 -0300

    add exercicio 023

---
+++ exe023.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+
+num = input('Digite um número de 0 a 9999: ')
+print('Unidade: ', num[3])
+print('Dezena: ', num[2])
+print('Centena: ', num[1])
+print('Milhar: ', num[0])
