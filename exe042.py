0f094a3b3c7b8ae8468d510ba5846ccc0a05e559
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-25 18:16:16 -0300

    update exercicio 042

--- exe042.py
+++ exe042.py
@@ -6,11 +6,11 @@ lado2 = float(input('Digite o valor do segundo lado: '))
 lado3 = float(input('Digite o valor do terceiro lado: '))
 
 if (lado1 < lado2 + lado3) and (lado2 < lado1 + lado3) and (lado3 < lado1 + lado2):
-    if lado1 == lado2 and lado1 == lado3 and lado2 == lado3:
+    if lado1 == lado2 == lado3:
         print('Esses valores formam um triângulo EQUILÁTERO!')
     elif (lado1 == lado2 and lado1 != lado3) or (lado1 == lado3 and lado1 != lado2) or (lado2 == lado3 and lado2 != lado1):
         print('Esses valores formam um triângulo ISÓSCELES!')
-    elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
+    elif lado1 != lado2 != lado3 != lado1:
         print('Esses valores formam um triângulo ESCALENO!')
 else:
     print('Esses valores não podem formar nenhum triângulo!')
