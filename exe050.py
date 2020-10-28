1c857b722144035b6902bd7bdd6168a168600ed8
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-28 13:55:03 -0300

    update exercicio 050

--- exe050.py
+++ exe050.py
@@ -9,5 +9,5 @@ for c in range(1,7):
     if num % 2 == 0:
         soma += num
         contador += 1
-print('Você digitou {} números PARES e a soma desses pares é {}.'.format(contador, soma))
+print('Você digitou {} números PARES e a soma é {}.'.format(contador, soma))
 print('FIM.')
