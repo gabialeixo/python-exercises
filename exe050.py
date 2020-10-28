bf6d521fe937797a075916732d5b774a957870a9
Gabriele Aleixo <gabealeixo13@gmail.com>
2020-10-28 11:41:58 -0300

    update exercicio 056

--- exe050.py
+++ exe050.py
@@ -2,9 +2,12 @@
 #digitado for ímpar, desconsidere-o.
 
 soma = 0
+contador = 0
 
-for c in range(0,7):
-    if c % 2 == 0:
-        soma += c
-print('A soma dos pares é {}.'.format(soma))
+for c in range(1,7):
+    num = int(input('Digite o {}º valor: '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        contador += 1
+print('Você digitou {} números PARES e a soma desses pares é {}.'.format(contador, soma))
 print('FIM.')
